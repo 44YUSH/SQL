1226bfe58f285b519c4193cf24960fe3589a646a
Aayush Sith <151053890+44YUSH@users.noreply.github.com>
2026-01-28 23:01:43 +0530

    Time: 388 ms (8.99%) | Memory: 0B (100.00%) - LeetSync

--- 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -1,16 +1,11 @@
-select a.user_id, trial_avg_duration, paid_avg_duration 
-from (
-    select user_id,
-    round(avg(activity_duration),2) as trial_avg_duration
-    from UserActivity
-    where activity_type="free_trial"
-    group by user_id
-    order by user_id) as a
-right join (
-    select user_id,
-    round(avg(activity_duration),2) as paid_avg_duration
-    from UserActivity
-    where activity_type="paid"
-    group by user_id
-    order by user_id) as b
-on a.user_id=b.user_id;
+select
+    user_id,
+    round(avg(case when activity_type = 'free_trial' then activity_duration end), 2) as trial_avg_duration,
+    round(avg(case when activity_type = 'paid' then activity_duration end), 2) as paid_avg_duration
+from useractivity
+group by user_id
+having
+    sum(case when activity_type = 'free_trial' then 1 else 0 end) > 0
+    and
+    sum(case when activity_type = 'paid' then 1 else 0 end) > 0
+order by user_id;
